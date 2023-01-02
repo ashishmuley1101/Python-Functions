0c5ef8adfb4aa1ffe776919c572d0d18840eecd7
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-02 09:53:04 +0530

    Python functions with argument

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def area(radius):
+    result = 3.14 * (radius * radius)
+    return result
+
+circle_radius = int(input("Enter the circle radius : "))
+
+print("Area of circle is : ", area(circle_radius))
+
+
