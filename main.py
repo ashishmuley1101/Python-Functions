bf3dfc5f2b59b6a503fdd75ef96994a117d5721f
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-02 09:59:48 +0530

    Python functions argument with default values

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
-
-def area(radius):
-    result = 3.14 * (radius * radius)
-    return result
-
-circle_radius = int(input("Enter the circle radius : "))
-
-print("Area of circle is : ", area(circle_radius))
+def add_numbers( a = 7,  b = 8):
+    sum = a + b
+    print('Sum:', sum)
 
 
+# function call with two arguments
+add_numbers(2, 3)
+
+#  function call with one argument
+add_numbers(a = 2)
+
+# function call with no arguments
+add_numbers()
