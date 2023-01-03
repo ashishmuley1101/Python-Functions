5c79c77563c1b7bb56dbf0af26548fb80c3ad450
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-03 10:28:01 +0530

    Program Square for the given numbers using lambda and map() function.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 
-# Square for the given numbers using lambda function
+# Square for the given numbers using lambda and map() function
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-square_numbers = []
 
-square = lambda n : n ** 2
+# map() returning the numbers object we have to convert map() into list()
+square = list(map(lambda n : n ** 2, numbers))
 
-for n in numbers:
-    square_numbers.append(square(n))
-
-print("Square of numbers : ", square_numbers)
+print("Square of numbers : ", square)
